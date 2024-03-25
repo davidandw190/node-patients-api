1bb2cdff2670e70a3c90e5406e33e9243da02156
Andrei-David Nan <nandavid190@gmail.com>
2024-03-25 23:59:02 +0200

    v0.5 - improve patients-init.sql script;

--- dbinit/patients-init.sql
+++ dbinit/patients-init.sql
@@ -6,11 +6,11 @@ DROP TABLE IF EXISTS patients;
 
 CREATE TABLE patients (
   patient_id  BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  first_name  VARCHAR(255) DEFAULT NULL,
-  last_name   VARCHAR(255) DEFAULT NULL,
-  email       VARCHAR(255) DEFAULT NULL,
-  phone       VARCHAR(255) DEFAULT NULL,
-  address     VARCHAR(255) DEFAULT NULL,
+  first_name  VARCHAR(25)  NOT NULL,
+  last_name   VARCHAR(25)  NOT NULL,
+  email       VARCHAR(25)  NOT NULL,
+  phone       VARCHAR(10)  DEFAULT NULL,
+  address     VARCHAR(100) DEFAULT NULL,
   diagnosis   VARCHAR(255) DEFAULT NULL,
   image_url   VARCHAR(255) DEFAULT NULL,
   created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -20,17 +20,18 @@ CREATE TABLE patients (
 
 DELIMITER //
 CREATE PROCEDURE create_and_return(
-        IN first_name VARCHAR(255), 
-        IN last_name VARCHAR(255), 
-        IN email VARCHAR(255), 
-        IN phone VARCHAR(255), 
-        IN address VARCHAR(255), 
-        IN diagnosis VARCHAR(255),
-        IN image_url VARCHAR(255)
+        IN first_name VARCHAR(25), 
+        IN last_name  VARCHAR(25), 
+        IN email      VARCHAR(25), 
+        IN phone      VARCHAR(10), 
+        IN address    VARCHAR(100), 
+        IN diagnosis  VARCHAR(255),
+        IN image_url  VARCHAR(255)
 )
 
 BEGIN
-  INSERT INTO patients(first_name, last_name, email, phone, address, diagnosis, image_url) VALUES (first_name, last_name, email, phone, address, diagnosis, image_url);
+  INSERT INTO patients(first_name, last_name, email, phone, address, diagnosis, image_url) 
+  VALUES (first_name, last_name, email, phone, address, diagnosis, image_url);
   
   SET @PATIENT_ID = LAST_INSERT_ID();
 
