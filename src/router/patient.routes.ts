cd91162d46244532237c19f541e0c81c086e53c4
Andrei-David Nan <nandavid190@gmail.com>
2024-03-30 14:52:50 +0200

    v0.19 - configure patient routes;

--- src/router/patient.routes.ts
+++ src/router/patient.routes.ts
@@ -1,3 +1,14 @@
 import { Router } from "express";
+import { createPatient, deletePatient, getPatient, updatePatient } from "../resource/patient.resource";
 
 export const patientRoutes = Router();
+
+patientRoutes.route('/')
+    .get(getPatient)
+    .post(createPatient);
+
+patientRoutes.route('/:patientId')
+    .get(getPatient)
+    .put(updatePatient)
+    .delete(deletePatient);
+    
