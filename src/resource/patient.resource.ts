6bec385a2ef77b0aa7bfb890e2356646147ad2db
Andrei-David Nan <nandavid190@gmail.com>
2024-03-30 14:47:35 +0200

    v0.18 - configure patients update and deletion;

--- src/resource/patient.resource.ts
+++ src/resource/patient.resource.ts
@@ -74,7 +74,55 @@ export const createPatient = async (req: Request, res: Response): Promise<Respon
 			.send(new HttpResponse(Code.INTERNAL_SERVER_ERROR, Status.INTERNAL_SERVER_ERROR, 'An error occurred'));
 	}
 }
+
+export const updatePatient = async (req: Request, res: Response): Promise<Response<Patient>> => {
+	console.info(`[${new Date().toLocaleString()}] Incoming ${req.method}${req.originalUrl} Request from ${req.rawHeaders[0]} ${req.rawHeaders[1]}`);
+	let patientData: Patient = { ...req.body };
+
+	try {
+		const pool = await connection();
+		const result: ResultSet = await pool.query(QUERY.SELECT_PATIENT, [req.params.patientId]);
+
+		if ((result[0] as Array<ResultSet>).length > 0) {
+			await pool.query(QUERY.UPDATE_PATIENT, [...Object.values(patientData)])
+			return res.status(Code.OK)
+				.send(new HttpResponse(Code.OK, Status.OK, "Patient updated succesfully", { ...patientData, patient_id: req.params.patientId }));
+		
+		} else {
+			return res.status(Code.NOT_FOUND)
+				.send(new HttpResponse(Code.NOT_FOUND, Status.NOT_FOUND, 'Patient not found'));
+		}
+
+	} catch (error: unknown) {
+		console.error(error);
+		return res.status(Code.INTERNAL_SERVER_ERROR)
+      		.send(new HttpResponse(Code.INTERNAL_SERVER_ERROR, Status.INTERNAL_SERVER_ERROR, 'An error occurred'));
+  	}
+}
+
+export const deletePatient = async (req: Request, res: Response): Promise<Response<Patient>> => {
+	console.info(`[${new Date().toLocaleString()}] Incoming ${req.method}${req.originalUrl} Request from ${req.rawHeaders[0]} ${req.rawHeaders[1]}`);
 	
+	try {
+		const pool = await connection();
+		const result: ResultSet = await pool.query(QUERY.SELECT_PATIENT, [req.params.patientId]);
+		
+		if ((result[0] as Array<ResultSet>).length > 0) {
+			const result: ResultSet = await pool.query(QUERY.DELETE_PATIENT, [req.params.patientId]);
+			return res.status(Code.OK)
+        		.send(new HttpResponse(Code.OK, Status.OK, 'Patient deleted'));
+    	
+		} else {
+			return res.status(Code.NOT_FOUND)
+        		.send(new HttpResponse(Code.NOT_FOUND, Status.NOT_FOUND, 'Patient not found'));
+		}
+
+	} catch (error: unknown) {
+		console.error(error);
+		return res.status(Code.INTERNAL_SERVER_ERROR)
+			.send(new HttpResponse(Code.INTERNAL_SERVER_ERROR, Status.INTERNAL_SERVER_ERROR, 'An error occurred'));
+	}
+}	
 
 const _checkPatientEmailExists = async (pool: Pool, email: string): Promise<boolean> => {
 	const existingPatientCount: any = await pool.query(QUERY.CHECK_PATIENT_EXISTS_BY_EMAIL, [email])
